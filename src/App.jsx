aee4a29e45e6bf42d07011c0126efb155ebd2ffe
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-03-10 18:33:19 +0100

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -34,25 +34,24 @@ function App() {
       <h1 className='text-5xl font-[500] text-center underline'>MY TO-DO LIST</h1>
       <form onSubmit={handleSubmit} className='my-[7rem]'>
         <div className='w-[40%]'>
+          <div className='flex mb-[2rem]'>
           <input className='border border-white w-[27rem] h-[5rem] focus:outline-none text-black px-[1rem]' type="text" placeholder='Add your lists' value={input} onChange={(e) => setInput(e.target.value)} />
-      
-          <div className='my-[2rem] w-[27rem] flex justify-center gap-[1rem]'>
-            <button className='p-[1rem] border border-white rounded-md font-[500]' type='submit'>Add List</button> 
-            <button onClick={() => setToDoList([])} className='p-[1rem] border border-white rounded-md font-[500]'>Clear List</button>
+          <button className='p-[1.5rem] border border-white rounded-md font-[500]' type='submit'>Add</button> 
           </div>
+      
           <div className='flex gap-[2rem]'>
-            <p>Pending tasks</p>
+            <p>Pending tasks: {toDoList.length}</p>
             <p>Completed tasks</p>
           </div>
         </div>
 
-        <div className='w-[30rem] min-h-[0vh] flex text-black bg-white rounded-md'>
-          
-          <ul>
+        <div className='w-[30rem] min-h-[0vh] text-black bg-white rounded-md'>
             {toDoList.map((todo, index) => (
-              <li className='flex m-[1.5rem]' key={`${todo} - ${index}`}><input type="checkbox" className='mr-[1rem]' />{todo.task}</li>
+              <ul>
+                <input type="checkbox" className='mr-[1rem]' />
+              <li key={`${todo} - ${index}`}>{todo}</li>
+              </ul>
             ))}
-          </ul>
         </div>
       </form>
     </div>
