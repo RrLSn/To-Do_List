7bc8846142ebcc32175052439e017b7a50adc888
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-03-12 22:43:32 +0100

    Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,61 @@ function App() {
     setInput(e.target.value)
   }
 
+  const addTask = () => {
+  if(input !== ""){
+    const taskDetails = {
+      id: Math.floor(Math.random*1000),
+      value: input,
+      isCompleted: false,
+    }
+    setToDoList([...toDoList, taskDetails])
+    setInput('')
+  }
+  }
+
+  // const handleDelete = (e, id) => {
+  //   e.preventDefault();
+  //   setToDoList(toDoList.filter(t) )
+  // }
+
+  const handleComplete = (e, id) => {
+    e.preventDefault();
+
+    //Find index
+    const elements = toDoList.findIndex(c => c.id === id)
+
+    //copy array into new variable
+    const newTodoList = [...toDoList]
+
+    //edit elements
+    newTodoList[elements] = {
+      ...newTodoList[elements],
+      isCompleted: true
+    }
+
+    setToDoList(newTodoList)
+  }
+
   return (
     <div className="App bg-black w-[100vw] min-h-[100vh] text-white p-[4rem]">
       <h1 className='text-5xl font-[500] text-center underline'>MY TO-DO LIST</h1>
       <div className='my-[7rem]'>
         <div className='w-[40%]'>
           <div className='flex mb-[2rem]'>
-          <input type="text" placeholder='Add your lists' className='border border-white w-[27rem] h-[5rem] focus:outline-none text-black px-[1rem]' id='text' value={input} onChange={(e) => handleChange()} />
+          <input type="text" placeholder='Add your lists' className='border border-white w-[27rem] h-[5rem] focus:outline-none text-black px-[1rem]' id='text' value={input} onChange={(e) => handleChange(e)} />
           <button className='p-[1.5rem] border border-white rounded-md font-[500]' onClick={addTask}>Add</button> 
           </div>
       
-          {/* <div className='flex gap-[2rem]'>
+          <div className='flex gap-[2rem]'>
             <p>Pending tasks: {toDoList.length}</p>
             <p>Completed tasks</p>
-          </div> */}
+          </div>
         </div>
 
         <div className='w-[30rem] min-h-[0vh] text-black bg-white rounded-md'>
           {toDoList.map((todo,index) => (
-          <ul key={index} className='flex justify-between p-[1rem]'>
-            <input type="checkbox"/>
+          <ul key={index} className={`flex justify-between p-[1rem] ${todo.isCompleted? "holdtext" : "listItem"}`}>
+            <input type="checkbox" />
             <li>{todo.value}</li>
             <button className='bg-black text-white rounded-md p-[0.3rem]'>Delete</button>
           </ul>
@@ -40,18 +75,3 @@ function App() {
 }
 
 export default App
-
-
-
-// const addTask = () => {
-//   if(input !== ""){
-//     const taskDetails = {
-//       id: Math.floor(Math.random*1000),
-//       value: input,
-//       isCompleted: false,
-//     }
-//     setToDoList([...toDoList, taskDetails])
-//     setInput('')
-//   }
-  
-// }
