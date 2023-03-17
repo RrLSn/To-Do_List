69cbec9a1d442d5deb55bbf1cec8b5496da31e50
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-03-17 23:03:39 +0100

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,34 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [input, setInput] = useState("")
-  const [toDoList, setToDoList] = useState([])
-  const [CompletedTask, setCompletedTask] = useState(false)
+  const [todos, setTodos] = useState("")
+  const [toDoList, setToDoList] = useState([
+    { todo: "Eat", 
+      isCompleted: true, 
+      id: Math.floor(Math.random()*1000)
+    }])
   const [checkBox, setCheckBox] = useState(0)
 
   const handleChange = (e) => {
-    setInput(e.target.value)
+    setTodos(e.target.value)
   }
 
-  const addTask = () => {
-  if(input !== ""){
-    const taskDetails = {
-      id: Math.floor(Math.random() * 1000),
-      value: input,
-      isCompleted: false,
-    }
-    setToDoList([...toDoList, taskDetails])
-    setInput('')
-  }
+  const addTask = (e) => {
+  e.preventDefault();
+  const newTodoList = toDoList.concat({ todo: todos, isCompleted: false, id: Math.floor(Math.random()*1000)})
+  setToDoList(newTodoList)
+  setTodos('')
   }
   
-  const handleComplete = (e, id) => {
-    e.preventDefault();
-
-    //Find index
-    const elements = toDoList.findIndex(element => element.id == id);
-
-    //copy array into new variable
+  const handleComplete = (element) => {
     const newTodoList = [...toDoList]
-
-    //edit elements
-    newTodoList[elements] = {
-      ...newTodoList[elements],
-      isCompleted: CompletedTask,
+    newTodoList[element] = {
+      todo: newTodoList[element].todo,
+      isCompleted: !newTodos[element].isCompleted,
+      id: newTodoList[element].id
     }
-
+    console.log(newTodoList)
     setToDoList(newTodoList)
-    // console.log(newTodoList[elements])
-    // console.log(newTodoList)
   }
 
   const deleteTask = (e, id) => {
@@ -48,17 +37,10 @@ function App() {
     setToDoList(toDoList.filter((t) => t.id != id))
   }
 
-  const handlecheckbox = (e) => {
-    e.preventDefault();
-    if(e.target.checked){
-      setCheckBox(checkBox + 1)
-    }
-    else{
-      setCheckBox(checkBox - 1)
-    }
-  }
 
-  // console.log(toDoList)
+  // const handlecheckbox = () => {
+  //   toDoList.isCompleted? setCheckBox(checkBox + 1) : setCheckBox(checkBox - 1)
+  // }
 
   
 
@@ -67,10 +49,10 @@ function App() {
       <h1 className='text-5xl font-[500] text-center underline'>MY TO-DO LIST</h1>
       <div className='my-[7rem]'>
         <div className='w-[40%]'>
-          <div className='flex mb-[2rem]'>
-            <input type="text" placeholder='Add your lists' className='border border-white w-[27rem] h-[5rem] focus:outline-none text-black px-[1rem]' id='text' value={input} onChange={(e) => handleChange(e)} />
-            <button className='p-[1.5rem] border border-white rounded-md font-[500]' onClick={addTask}>Add</button> 
-          </div>
+          <form onSubmit={addTask} className='flex mb-[2rem]'>
+            <input type="text" placeholder='Add your lists' className='border border-white w-[27rem] h-[5rem] focus:outline-none text-black px-[1rem]' id='text' value={todos} onChange={(e) => handleChange(e)} />
+            <button className='p-[1.5rem] border border-white rounded-md font-[500]'>Add</button> 
+          </form>
       
           <div className='flex gap-[2rem]'>
             <p>Pending tasks: {toDoList.length - checkBox}</p>
@@ -79,25 +61,19 @@ function App() {
         </div>
 
         <div className='w-[30rem] min-h-[0vh] text-black bg-white rounded-md'>
-          {toDoList.map((todo,index) => (
-          <ul key={index} className={`flex justify-between p-[1rem]`}>
+          {toDoList.map((tl,index) => (
+          <div key={index} className={`flex justify-between p-[1rem]`}>
 
             <input type="checkbox" 
-            checked={todo.isCompleted}
-            onChange={(e) => { handlecheckbox(e),
-              handleComplete(e, todo.id) 
-              setCompletedTask(!CompletedTask)}
-            } 
+            checked={tl.isCompleted}
             value={checkBox} 
-            // onClick={(e) => {
-            //     handleComplete(e, todo.id) 
-            //     setCompletedTask(!CompletedTask)}} 
-                />  
-            <li className={todo.isCompleted ? 'holdtext' : "text"}>{todo.value}</li>
+            onChange={handleComplete}
+            />  
+            <span className={tl.isCompleted ? 'holdtext' : "text"}>{tl.todo}</span>
 
-            <button className='bg-[red] text-white rounded-md p-[0.3rem] delete' onClick={(e) => deleteTask(e,todo.id)}>Delete</button>
+            <button className='bg-[red] text-white rounded-md p-[0.3rem] delete' onClick={(e) => deleteTask(e,tl.id)}>Delete</button>
 
-          </ul>
+          </div>
             ))}
         </div>
       </div>
